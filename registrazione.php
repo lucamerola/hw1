38e887e6476ddaec0f127241d5b14905ed7fed7c
lucamerola <luca.merola@studium.unict.it>
2022-05-18 14:16:15 +0200

    coomit in master

---
+++ registrazione.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="registrazione.css">
+    <title>Registrazione</title>
+</head>
+<body>
+    <?php include("menu.html"); ?>
+</body>
+</html>
