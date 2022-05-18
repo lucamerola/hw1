bae2f1c73919c71d7d6750ac47e7f55fdba6a18a
lucamerola <luca.merola@studium.unict.it>
2022-05-18 15:37:23 +0200

    commit in master

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="hw1.css">
     <link rel="stylesheet" href="menu.css">
-    <script src="script.js" defer="true"></script>
+    <script src="script-top-5-cocktail.js" defer="true"></script>
     <title>Home</title>
 </head>
 <body>
